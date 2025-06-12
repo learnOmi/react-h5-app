637422b3ded5c69a61ead6b48fd9a1498eb81bbf
Tong <1427275295@qq.com>
2025-06-13 01:30:47 +0800

    basic routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import { Button } from "antd-mobile";
+import React, { Suspense } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom"; 
+//  动态导入（import()）要求路径必须是静态字符串; Webpack 在构建时无法解析变量或非字面量路径中的别名
+const Home = React.lazy(()=> import(`@/pages/Home`));
+const Login = React.lazy(()=> import(`@/pages/Login`));
+
 
 function App() {
   return (
-    <Button block color='primary' size='large'>
-      Block Button
-    </Button>
+    <BrowserRouter>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/home" element={<Home />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
   );
 }
 
