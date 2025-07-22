12da6be084b54bf7ca4fac0f8d879060a5e28839
Tong <1427275295@qq.com>
2025-07-22 23:12:24 +0800

    delete channels in Channel Panel

--- src/pages/Home/Channel/index.js
+++ src/pages/Home/Channel/index.js
@@ -1,16 +1,47 @@
 import Icon from "@/components/Icon"
-import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import styles from './index.module.scss'
 import { differenceBy } from "lodash"
 import classNames from "classnames"
+import { useState } from "react"
+import { delChannel } from "@/store/actions/home"
+import { Toast } from "antd-mobile"
 
 const Channels = ({ onClose, index, onChange }) => {
+  const [editing, setEditing] = useState(false);
+  const dispatch = useDispatch();
   const userChannels = useSelector(state => state.home.userChannels)
   // 推荐频道
   const recommendChannels = useSelector(state => {
     const { userChannels, allChannels } = state.home
     return differenceBy(allChannels, userChannels, 'id')
   })
+
+  const changeChannel = (i) => {
+    if (editing) return;
+    onChange(i);
+    onClose();
+  }
+
+    // 删除频道
+  const del = (channel,i) => {
+    if(userChannels.length <= 4) {
+      Toast.info('请至少保留4个频道哦');
+      return;
+    }
+    dispatch(delChannel(channel))
+    // 删除的时候处理高亮
+    // 高亮处理
+    // 1.如果删除的 i 和 index 相等，则默认推荐 0
+    // 2.小于 index ，默认让i - 1 高亮
+    // 3.大于index ， 不处理
+    if(i === index) {
+      onChange(0);
+    }
+    if(i<index) {
+      onChange(index - 1);
+    } 
+  }
 
   return (
     <div className={styles.root}>
@@ -21,10 +52,19 @@ const Channels = ({ onClose, index, onChange }) => {
       {/* 频道列表 */}
       <div className="channel-content">
         {/* 当前已选择的频道列表 */}
-        <div className={classNames('channel-item')}>
+        <div className={classNames('channel-item', { edit : editing })}>
           <div className="channel-item-header">
             <span className="channel-item-title">我的频道</span>
-
+            <span className="channel-item-title-extra">
+              {
+                editing ? '点击删除频道' : '点击进入频道'
+              }
+            </span>
+            <span className="channel-item-edit" onClick={() => setEditing(!editing)}>
+              {
+                editing ? '保存' : '编辑'
+              }
+            </span>
           </div>
           <div className="channel-list">
             {
@@ -33,9 +73,12 @@ const Channels = ({ onClose, index, onChange }) => {
                   selected: index === i
                 })}
                   key={item.id}
-                  onClick={() => {onChange(i); onClose()}}
+                  onClick={changeChannel}
                 >
                   {item.name}
+                  {item.id !== 0 && (
+                    <Icon type='icon-Clear1' onClick={() => del(item,i)} />
+                  )}
                 </span>
               ))
             }
--- src/store/actions/home.js
+++ src/store/actions/home.js
@@ -46,3 +46,27 @@ export const saveChannels = (data) => {
         payload: data
     }
 }
+
+// 删除频道
+export const delChannel = (channel) => {
+  return async (dispatch, getState) => {
+    // 如果用户登录了，需要发送请求删除频道
+    // 如果用户没有登录，需要删除本地的这个频道
+    // 不管有没有登录，都要修改redux的值
+    const userChannels = getState().home.userChannels
+    if (hasToken()) {
+      // 发送请求
+      await request.delete('/user/channels/' + channel.id)
+      // 同步频道的数据到redux中
+      dispatch(
+        saveChannels(userChannels.filter((item) => { return item.id !== channel.id }))
+      )
+    } else {
+      // 没有登录
+      // 修改本地，修改redux
+      const result = userChannels.filter((item) => { return item.id !== channel.id })
+      dispatch(saveChannels(result))
+      setChannels(result)
+    }
+  }
+}
