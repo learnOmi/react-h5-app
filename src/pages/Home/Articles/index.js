3bd388629eaf1cf0f8c49eb14fe1feb5daa05ecc
Tong <1427275295@qq.com>
2025-08-18 19:56:17 +0800

    pull To refresh ariticles

--- src/pages/Home/Articles/index.js
+++ src/pages/Home/Articles/index.js
@@ -3,18 +3,26 @@ import ArticleItem from '../Article'
 import styles from './index.module.scss'
 import { useEffect } from 'react';
 import { getArticleList } from '@/store/actions/home';
+import { PullToRefresh } from 'antd-mobile'
 
 const ArticleList = ({ channelId, activeId }) => {
   const dispatch = useDispatch();
   const list = useSelector(state => state.home.articles[activeId]?.list || []);
 
+  const onRefresh = async () => {
+    // 刷新
+    await dispatch(getArticleList(channelId, Date.now()))
+  }
+
   useEffect(()=>{
     if ( channelId !== activeId ) return;
+    if ( list.length > 0) return;
     dispatch(getArticleList(activeId, Date.now()));
-  }, [dispatch, activeId, channelId]);
+  }, [dispatch, activeId, channelId, list]);
 
   return (
     <div className={styles.root}>
+      <PullToRefresh onRefresh={onRefresh}>
         <div className='article-item'>
             {list.map((item) => {
                 return (
@@ -22,6 +30,7 @@ const ArticleList = ({ channelId, activeId }) => {
                 )
             })}
         </div>
+      </PullToRefresh>
     </div>
   )
 }
